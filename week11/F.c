00ffc863298e96cd25cb23bfe8513c45e7c06345
blackapodidae <blackapodidae@blackapodidae-M>
2018-12-03 16:36:02 +0800

    change folders name

--- week11/F.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a, b, c, d;
-    scanf("%d%d%d%d", &a, &b, &c, &d);
-    printf("%d %d %d %d\n", a + b, a + c + d, a, c);
-    printf("na=a+b\nnb=a+c+d\nnc=a\nnd=c");
-    return 0;
-}
